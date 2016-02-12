6c00b3a98b796544d1ec0072692636fec6fae159
Vitaly Aminev <v@aminev.me>
2016-02-12 13:32:25 +0300

    fix: check for bool property existence, not it's value

--- src/endpoints/planCreate.js
+++ src/endpoints/planCreate.js
@@ -105,7 +105,7 @@ exports.post = {
           const yearly = formSubscriptionObject('year', subscriptions.yearly);
 
           const message = {
-            hidden: plan.hidden || true,
+            hidden: 'hidden' in plan ? plan.hidden : true,
             alias: plan.alias || '',
             plan: {
               name: plan.name,
